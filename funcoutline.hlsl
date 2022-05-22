40721e2ddd009ff578ccc43836c91c59e4e51198
shudal <shudal@qq.com>
2022-05-22 10:09:26 +0800

    toon shader,outline时同时考虑normal和depth

--- funcoutline.hlsl
+++ funcoutline.hlsl
@@ -15,11 +15,34 @@ if (nowDep+1 > customDep) {
         for (int k=0;k<2;k++) { 
             float subx=oriCompUV.x + dx[i]*outSize.x;
             float suby=oriCompUV.y + dy[k]*outSize.y;
-            totalDis = totalDis + nowDep-SceneTextureLookup(float2(subx,suby), 1, false).x;
+            totalDis = totalDis + nowDep-SceneTextureLookup(float2(subx,suby), 1, false).x; 
         }
     } 
-    //totalDis = abs(totalDis);
+
+    totalDis = abs(totalDis);
+    
+    float2 oriCompUVForNormal=GetDefaultSceneTextureUV(Parameters, 8); 
+
+    float3 oriCompNor=SceneTextureLookup(oriCompUVForNormal, 8, false);
+    float3 totalDisVec = float3(0,0,0);
+    for (int i=0;i<4;i++) {
+        for (int k=0;k<2;k++) { 
+            float subx=oriCompUVForNormal.x + dx[i]*outSize.x;
+            float suby=oriCompUVForNormal.y + dy[k]*outSize.y; 
+            totalDisVec = totalDisVec + oriCompNor - SceneTextureLookup(float2(subx,suby), 8, false).xyz;
+        }
+    } 
+    totalDis = abs(totalDis);
+    totalDis = totalDis+length(totalDisVec);
     totalDis=pow(totalDis,2);
     ans=lerp(oriColor.rgb,float3(0,0,0),totalDis); 
+
+    float alpha2=0; 
+    #define PI 3.1415926
+    float tmpx=0.001*nowDep; 
+    alpha2 = min(cos(PI * tmpx / 2),1-abs(tmpx));
+    alpha2 = clamp(alpha2,0,1);
+    alpha2 = pow(alpha2,2);
+    ans=lerp(oriColor.rgb,ans,alpha2);
 }
 return ans;
